ac61c4268b568eb35c2c5251e969051617ebf006
Kevin Händel <kevinhaendel@gmail.com>
2021-06-11 23:04:47 +0200

    Added Inter font

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
+        fontFamily: 'Inter',
       ),
       home: MyHomePage(title: 'EU green certificate Demo'),
     );
