815062af5af7ac48c6f3326e023d00f01dae890a
Kevin Händel <kevinhaendel@gmail.com>
2021-06-20 00:29:26 +0200

    Improved app design

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:country_codes/country_codes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_statusbarcolor_ns/flutter_statusbarcolor_ns.dart';
 import 'package:greenpass_app/views/demo_page.dart';
 import 'package:greenpass_app/views/qr_code_scanner.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -37,11 +38,8 @@ class MyHomePage extends StatefulWidget {
 
 class _HomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
 
-  List<Widget> _tabPages = [
-    DemoPage(),
-    QRCodeScanner(),
-  ];
   int _currentPageIdx = 0;
+  static const int _pageCount = 2;
 
   late TabController _tabController;
   static const platform = const MethodChannel('eu.greenpassapp.wallet');
@@ -74,20 +72,45 @@ class _HomePageState extends State<MyHomePage> with SingleTickerProviderStateMix
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: _tabPages.length, vsync: this);
+    _tabController = TabController(length: _pageCount, vsync: this);
     _tabController.addListener(() {
       setState(() { _currentPageIdx = _tabController.index; });
     });
 
     _tabController.animation!.addListener(() {
       int roundedIdx = _tabController.animation!.value.round();
-      if (_currentPageIdx != roundedIdx)
+      if (_currentPageIdx != roundedIdx) {
         setState(() { _currentPageIdx = roundedIdx; });
+
+        if (_currentPageIdx == 0) {
+          FlutterStatusbarcolor.setStatusBarWhiteForeground(false);
+        } else {
+          FlutterStatusbarcolor.setStatusBarWhiteForeground(true);
+        }
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> _tabPages = [
+      DemoPage(),
+      Stack(
+        children: [
+          QRCodeScanner(),
+          Container(
+            color: Colors.black45,
+            alignment: Alignment.topCenter,
+            height: MediaQuery.of(context).padding.top + kToolbarHeight,
+          ),
+        ],
+      ),
+    ];
+
+    FlutterStatusbarcolor.setStatusBarColor(Colors.transparent);
+    FlutterStatusbarcolor.setNavigationBarWhiteForeground(Theme.of(context).brightness == Brightness.dark);
+    FlutterStatusbarcolor.setNavigationBarColor(Theme.of(context).scaffoldBackgroundColor);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -106,7 +129,7 @@ class _HomePageState extends State<MyHomePage> with SingleTickerProviderStateMix
             color: _currentPageIdx == 0 ? Colors.black : Colors.white,
             onPressed: () async {
               //var encodedCert = Uri.encodeFull("NCFTW2H:7*I06R3W/J:O6:P4QB3+7RKFVJWV66UBCE//UXDT:*ML-4D.NBXR+SRHMNIY6EB8I595+6UY9-+0DPIO6C5%0SBHN-OWKCJ6BLC2M.M/NPKZ4F3WNHEIE6IO26LB8:F4:JVUGVY8*EKCLQ..QCSTS+F\$:0PON:.MND4Z0I9:GU.LBJQ7/2IJPR:PAJFO80NN0TRO1IB:44:N2336-:KC6M*2N*41C42CA5KCD555O/A46F6ST1JJ9D0:.MMLH2/G9A7ZX4DCL*010LGDFI\$MUD82QXSVH6R.CLIL:T4Q3129HXB8WZI8RASDE1LL9:9NQDC/O3X3G+A:2U5VP:IE+EMG40R53CG9J3JE1KB KJA5*\$4GW54%LJBIWKE*HBX+4MNEIAD\$3NR E228Z9SS4E R3HUMH3J%-B6DRO3T7GJBU6O URY858P0TR8MDJ\$6VL8+7B5\$G CIKIPS2CPVDK%K6+N0GUG+TG+RB5JGOU55HXDR.TL-N75Y0NHQTZ3XNQMTF/ZHYBQ\$8IR9MIQHOSV%9K5-7%ZQ/.15I0*-J8AVD0N0/0USH.3");
-             var cert = "NCFTW2H%3A7*I06R3W%2FJ%3AO6%3AP4QB3%2B7RKFVJWV66UBCE%2F%2FUXDT%3A*ML-4D.NBXR%2BSRHMNIY6EB8I595%2B6UY9-%2B0DPIO6C5%250SBHN-OWKCJ6BLC2M.M%2FNPKZ4F3WNHEIE6IO26LB8%3AF4%3AJVUGVY8*EKCLQ..QCSTS%2BF%24%3A0PON%3A.MND4Z0I9%3AGU.LBJQ7%2F2IJPR%3APAJFO80NN0TRO1IB%3A44%3AN2336-%3AKC6M*2N*41C42CA5KCD555O%2FA46F6ST1JJ9D0%3A.MMLH2%2FG9A7ZX4DCL*010LGDFI%24MUD82QXSVH6R.CLIL%3AT4Q3129HXB8WZI8RASDE1LL9%3A9NQDC%2FO3X3G%2BA%3A2U5VP%3AIE%2BEMG40R53CG9J3JE1KB%20KJA5*%244GW54%25LJBIWKE*HBX%2B4MNEIAD%243NR%20E228Z9SS4E%20R3HUMH3J%25-B6DRO3T7GJBU6O%20URY858P0TR8MDJ%246VL8%2B7B5%24G%20CIKIPS2CPVDK%25K6%2BN0GUG%2BTG%2BRB5JGOU55HXDR.TL-N75Y0NHQTZ3XNQMTF%2FZHYBQ%248IR9MIQHOSV%259K5-7%25ZQ%2F.15I0*-J8AVD0N0%2F0USH.3";
+              var cert = "NCFTW2H%3A7*I06R3W%2FJ%3AO6%3AP4QB3%2B7RKFVJWV66UBCE%2F%2FUXDT%3A*ML-4D.NBXR%2BSRHMNIY6EB8I595%2B6UY9-%2B0DPIO6C5%250SBHN-OWKCJ6BLC2M.M%2FNPKZ4F3WNHEIE6IO26LB8%3AF4%3AJVUGVY8*EKCLQ..QCSTS%2BF%24%3A0PON%3A.MND4Z0I9%3AGU.LBJQ7%2F2IJPR%3APAJFO80NN0TRO1IB%3A44%3AN2336-%3AKC6M*2N*41C42CA5KCD555O%2FA46F6ST1JJ9D0%3A.MMLH2%2FG9A7ZX4DCL*010LGDFI%24MUD82QXSVH6R.CLIL%3AT4Q3129HXB8WZI8RASDE1LL9%3A9NQDC%2FO3X3G%2BA%3A2U5VP%3AIE%2BEMG40R53CG9J3JE1KB%20KJA5*%244GW54%25LJBIWKE*HBX%2B4MNEIAD%243NR%20E228Z9SS4E%20R3HUMH3J%25-B6DRO3T7GJBU6O%20URY858P0TR8MDJ%246VL8%2B7B5%24G%20CIKIPS2CPVDK%25K6%2BN0GUG%2BTG%2BRB5JGOU55HXDR.TL-N75Y0NHQTZ3XNQMTF%2FZHYBQ%248IR9MIQHOSV%259K5-7%25ZQ%2F.15I0*-J8AVD0N0%2F0USH.3";
               await _pkPassDownload(url:"http://localhost:8081/api/user/pass?cert=$cert");
               //_addPassIntoWallet();
               ScaffoldMessenger.of(context).showSnackBar(
--- lib/views/qr_code_scanner.dart
+++ lib/views/qr_code_scanner.dart
@@ -1,6 +1,9 @@
+import 'package:app_settings/app_settings.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:greenpass_app/green_validator/green_validator.dart';
+import 'package:qr_mobile_vision/qr_mobile_vision.dart';
 import 'package:vibration/vibration.dart';
 import 'package:qr_mobile_vision/qr_camera.dart';
 
@@ -15,6 +18,58 @@ class _QRCodeScannerState extends State<QRCodeScanner> {
   @override
   Widget build(BuildContext context) {
     return QrCamera(
+      formats: [
+        BarcodeFormats.QR_CODE
+      ],
+      onError: (context, err) {
+        if (_isNoPermissionError(err as PlatformException)) {
+          return SafeArea(
+            minimum: const EdgeInsets.all(25.0),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.videocam_off,
+                    color: Theme.of(context).disabledColor,
+                    size: 60.0
+                  ),
+                  const Padding(padding: const EdgeInsets.symmetric(vertical: 6.0)),
+                  Text(
+                    'No Permission',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Padding(padding: const EdgeInsets.symmetric(vertical: 2.0)),
+                  Text(
+                    'Please allow GreenPass to access your camera in order to scan QR codes.',
+                    textAlign: TextAlign.center,
+                  ),
+                  const Padding(padding: const EdgeInsets.symmetric(vertical: 6.0)),
+                  OutlinedButton(
+                    child: Text('Go to app settings'),
+                    onPressed: () => AppSettings.openAppSettings(),
+                  ),
+                ],
+              ),
+            ),
+          );
+        } else {
+          return SafeArea(
+            minimum: const EdgeInsets.all(25.0),
+            child: Center(
+              child: Text(
+                "We're sorry, but there seems to be a problem to access your camera.",
+                textAlign: TextAlign.center,
+              ),
+            )
+          );
+        }
+      },
+      notStartedBuilder: (context) => Container(color: Theme.of(context).scaffoldBackgroundColor),
       qrCodeCallback: (code) {
         Vibration.vibrate(pattern: [0, 50]);
         showDialog(
@@ -27,5 +82,14 @@ class _QRCodeScannerState extends State<QRCodeScanner> {
         );
       },
     );
+  }
+
+  // helper method since the package only returns platform-specific exceptions
+  bool _isNoPermissionError(PlatformException ex) {
+    if (ex.code == 'PERMISSION_DENIED') return true; // iOS
+    else if (ex.code == 'QRREADER_ERROR') {
+      if (ex.message == 'noPermission' || ex.message == 'noPermissions') return true; // Android
+    }
+    return false; // unknown error
   }
 }
