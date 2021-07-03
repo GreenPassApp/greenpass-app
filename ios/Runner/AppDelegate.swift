213ad2e00b991ec18f3302325600cca2c1a53cf6
Jakob Stadlhuber <office@jakobstadlhuber.com>
2021-07-03 21:45:24 +0200

    added ios specific code

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,42 +1,13 @@
 import UIKit
 import Flutter
-import PassKit
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate, PKAddPassesViewControllerDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-        let batteryChannel = FlutterMethodChannel(name: "eu.greenpassapp.wallet", binaryMessenger: controller.binaryMessenger)
-        batteryChannel.setMethodCallHandler({
-          [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
-          // Note: this method is invoked on the UI thread.
-          guard call.method == "addPassIntoWallet" else {
-            result(FlutterMethodNotImplemented)
-            return
-          }
-            if let args = call.arguments as? Dictionary<String, String>,
-               let param = args["uri"] {
-                self?.addPass(result: result, uri: param)
-
-             } else {
-               result(FlutterError.init(code: "errorSetDebug", message: "data or format error", details: nil))
-             }
-        })
-
-        GeneratedPluginRegistrant.register(with: self)
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-    
-    private func addPass(result: FlutterResult, uri: String) {
-        if #available(iOS 10.0, *) {
-            UIApplication.shared.open(URL(string: uri)!)
-            result(true)
-
-        }else {
-            result(false)
-        }
-    }
 }
