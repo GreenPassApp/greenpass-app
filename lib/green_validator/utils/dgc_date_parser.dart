1f44e1b06a00b994e9de80029399c646034227d1
Kevin Händel <kevinhaendel@gmail.com>
2022-02-16 12:10:02 +0100

    Fix handling of epoch time given as double

--- lib/green_validator/utils/dgc_date_parser.dart
+++ lib/green_validator/utils/dgc_date_parser.dart
@@ -1,9 +1,7 @@
-import 'package:intl/intl.dart';
-
 class DGCDateParser {
   // calculate DateTime from cbor web token fields (comparable to JWT)
-  static DateTime fromCwtSeconds(int cwtSeconds) {
-    return DateTime.fromMillisecondsSinceEpoch(cwtSeconds * 1000, isUtc: true).toLocal();
+  static DateTime fromCwtSeconds(num cwtSeconds) {
+    return DateTime.fromMillisecondsSinceEpoch(cwtSeconds.toInt() * 1000, isUtc: true).toLocal();
   }
 
   // convert ISO 8601 formatted dates (and times) to DateTime instance
